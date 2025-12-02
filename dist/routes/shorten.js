4648e12640af4ee621d17dae40437ac7308a0daf
Mihail <mihailsorin58@gmail.com>
2025-12-02 12:44:43 +0500

    rm dist

--- dist/routes/shorten.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var urlController_1 = require("../controllers/urlController");
-var router = (0, express_1.Router)();
-router.post("/", urlController_1.urlShort);
-router.get("/:short", urlController_1.urlRedirect);
-exports.default = router;
-//# sourceMappingURL=shorten.js.map
